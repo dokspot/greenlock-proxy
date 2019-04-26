33c65c4e98c2a77897288ba5eae71ca039ce4b68
Cyrille <c.derche@me.com>
2019-04-26 13:31:13 +0200

    proxy created.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+import { Greenlock } from 'greenlock';
+let httpProxy = require('http-proxy');
+let proxy = httpProxy.createProxyServer({});
+let redir = require('redirect-https')();
+
+// let's create ou greenlock server first
+
+let greenlock = Greenlock.create({
+  // check greenlock's doc for the entire code sample
+});
+
+
+require('http').createServer(greenlock.middleware(redir)).listen(80);
+
+require('https').createServer(greenlock.tlsOptions, function (req: any, res: any) {
+  return proxy.web(req, res, {
+    target: 'https://another-server.com',
+  });
+}).listen(443);
---
+++ types/greenlock.d.ts
@@ -0,0 +1,19 @@
+// Circular reference from index
+export const Greenlock: any;
+// Circular reference from index
+export const LE: any;
+export const acmeChallengePrefix: string;
+export const challengeType: string;
+export const challengeTypes: string[];
+export function create(gl: any): any;
+export const defaults: {
+    acmeChallengePrefix: string;
+    challengeType: string;
+    challengeTypes: string[];
+    productionServerUrl: string;
+    rsaKeySize: number;
+    stagingServerUrl: string;
+};
+export const productionServerUrl: string;
+export const rsaKeySize: number;
+export const stagingServerUrl: string;
