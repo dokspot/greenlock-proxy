29baa2b8d45f6bb44fe5f2a85cc5a3ba71d6110d
Cyrille <c.derche@me.com>
2019-04-27 15:40:32 +0200

    mods

--- src/app.ts
+++ src/app.ts
@@ -14,11 +14,20 @@ let greenlock = Greenlock.create({
 , configDir: '~/.config/acme/'
 });
 
+let acmeChallengeHandler = greenlock.middleware(function (req: any, res: any) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end('<h1>Hello, ⚠️ Insecure World!</h1><a>Visit Secure Site</a>');
+});
+
+require('http').createServer(acmeChallengeHandler).listen(80, function (this: any) {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
+
 let spdyOptions = Object.assign({}, greenlock.tlsOptions);
 spdyOptions.spdy = { protocols: ['h2', 'http/1.1'], plain: false };
 let server = require('spdy').createServer(spdyOptions, function (req: any, res: any) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1>Hello, World!</h1>');
+  res.end('<h1>Hello, 🔐 Secure World!</h1>');
 });
 server.on('error', function (err: any) {
   console.error(err);
@@ -26,36 +35,10 @@ server.on('error', function (err: any) {
 server.on('listening', function (this: any) {
   console.log("Listening for SPDY/http2/https requests on", this.address());
 })
-server.listen(80, 443);
+server.listen(443);
 
-
-
-// // let redir = require('redirect-https')();
-// let acmeChallengeHandler = greenlock.middleware(function (req: any, res: any) {
-//   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-//   res.end('<h1>Hello, ⚠️ Insecure World!</h1><a>Visit Secure Site</a>');
-// });
-
-// require('http').createServer(acmeChallengeHandler).listen(80, function (this: any) {
-//   console.log("Listening for ACME http-01 challenges on", this.address());
-// });
-
-// let spdyOptions = Object.assign({}, greenlock.tlsOptions);
-// spdyOptions.spdy = { protocols: ['h2', 'http/1.1'], plain: false };
-// let server = require('spdy').createServer(spdyOptions, function (req: any, res: any) {
-//   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-//   res.end('<h1>Hello, 🔐 Secure World!</h1>');
-// });
-// server.on('error', function (err: any) {
-//   console.error(err);
-// });
-// server.on('listening', function (this: any) {
-//   console.log("Listening for SPDY/http2/https requests on", this.address());
-// })
-// server.listen(443);
-
-// // require('https').createServer(greenlock.tlsOptions, function (req: any, res: any) {
-// //   return proxy.web(req, res, {
-// //     target: 'https://www.google.com/',
-// //   });
-// // }).listen(443);
+// require('https').createServer(greenlock.tlsOptions, function (req: any, res: any) {
+//   return proxy.web(req, res, {
+//     target: 'https://www.google.com/',
+//   });
+// }).listen(443);
