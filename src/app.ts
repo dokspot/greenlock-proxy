79b66acfdfd5bcbcf77897957429ba7223b6db85
Cyrille <c.derche@me.com>
2019-04-27 18:36:40 +0200

    greenlock-express

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,42 @@
-let Greenlock = require('greenlock');
-let httpProxy = require('http-proxy');
-let proxy = httpProxy.createProxyServer({});
+let Greenlock = require('greenlock-express');
 
-// Storage Backend
-var leStore = require('greenlock-store-fs').create({
-  configDir: '/home/node/acme/etc'
-, webrootPath: '/tmp/acme-challenges'
-, debug: true
-});
+let glx = Greenlock.create({
+  version:          'draft-11'
+, server:           'https://acme-staging-v02.api.letsencrypt.org/directory'
+, email:            'cyrille.derche@dokspot.com'
+, agreeTos:         true
+, approvedDomains:  [ 'slave.clientdomain1.com', 'slave.clientdomain2.com', 'example.com' ]
+, configDir:        '/home/node/acme/'
+, communityMember:  true
+, debug:            true
+})
 
-// ACME Challenge Handlers
-// var http01Challenge = require('le-challenge-fs').create({
-//   webrootPath: '/home/node/acme/var'
-// , debug: true
-// });
+////////////////////////
+// http-01 Challenges //
+////////////////////////
 
-// let's create ou greenlock server first
-
-let greenlock = Greenlock.create({
-  version: 'draft-12'
-, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'
-, email: 'cyrille.derche@dokspot.com'
-, agreeTos: true
-, approvedDomains: [ 'slave.clientdomain1.com', 'slave.clientdomain2.com', 'example.com' ]
-, store: leStore
-// , challenges: {
-//   'http-01': http01Challenge
-// }
-, debug: true
-});
-
-let acmeChallengeHandler = greenlock.middleware(function (req: any, res: any) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1>Hello, ⚠️ Insecure World!</h1><a>Visit Secure Site</a>');
-});
-
+// http-01 challenge happens over http/1.1, not http2
+var redirectHttps = require('redirect-https')();
+var acmeChallengeHandler = glx.middleware(redirectHttps);
 require('http').createServer(acmeChallengeHandler).listen(80, function (this: any) {
   console.log("Listening for ACME http-01 challenges on", this.address());
 });
 
-let spdyOptions = Object.assign({}, greenlock.tlsOptions);
-spdyOptions.spdy = { protocols: ['h2', 'http/1.1'], plain: false };
-let server = require('spdy').createServer(spdyOptions, function (req: any, res: any) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1>Hello, 🔐 Secure World!</h1>');
+////////////////////////
+// http2 via SPDY h2  //
+////////////////////////
+
+// spdy is a drop-in replacement for the https API
+var spdyOptions = Object.assign({}, glx.tlsOptions);
+spdyOptions.spdy = { protocols: [ 'h2', 'http/1.1' ], plain: false };
+
+var server = require('spdy').createServer(spdyOptions, function(req: any, res: any) {
+  
 });
 server.on('error', function (err: any) {
   console.error(err);
 });
 server.on('listening', function (this: any) {
   console.log("Listening for SPDY/http2/https requests on", this.address());
-})
+});
 server.listen(443);
-
-// require('https').createServer(greenlock.tlsOptions, function (req: any, res: any) {
-//   return proxy.web(req, res, {
-//     target: 'https://www.google.com/',
-//   });
-// }).listen(443);
